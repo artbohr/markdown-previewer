e1e7d7b612582f01c811a7dddde062bdb55d936a
artbohr <26352035+artbohr@users.noreply.github.com>
2017-10-21 23:04:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# GitHub-flavored Markdown Previewer
+
+## Configuration:
+clone this repository and *run* these commands:
+
+```
+npm install
+npm start
+```
+
+### This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import marked from 'marked';
+import './App.css';
+//import {Grid} from 'react-bootstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markdown: "I am using __markdown__."
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({markdown: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 id="box_title">Markdown</h1>
+            <form>
+              <textarea rows="26" value={this.state.markdown} onChange={this.handleChange}/>
+            </form>
+          </div>
+          <div className="col-md-6">
+            <h1 id="box_title">Output</h1>
+            <p dangerouslySetInnerHTML={{
+              __html: marked(this.state.markdown)
+            }}/>
+          </div>
+        </div>
+      </div >
+    );
+  }
+}
+
+export default App;
